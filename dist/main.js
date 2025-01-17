2093271b15da961df69dcf5e9f6c3d52bf68fd6a
Juanma <daith043@gmail.com>
2025-01-17 21:56:06 +0100

    Add files via upload

--- dist/main.js
+++ dist/main.js
@@ -5,13 +5,11 @@ let next = document.querySelector("#next");
 let page = 0;
 let linkAPI = `https://pokeapi.co/api/v2/pokemon?limit=15&offset=${page}`;
 const container = document.getElementById("container-pkmn");
-// Variable global para pre-carga
-let nextPageData = null;
 let modal = document.createElement("div");
 modal.classList.add("modal");
 document.body.appendChild(modal);
 // EVENTOS
-previous === null || previous === void 0 ? void 0 : previous.addEventListener("click", () => {
+previous.addEventListener("click", () => {
     if (page === 0) {
         return;
     }
@@ -21,86 +19,79 @@ previous === null || previous === void 0 ? void 0 : previous.addEventListener("c
     fetchPokemons();
     hideNextPrev();
 });
-next === null || next === void 0 ? void 0 : next.addEventListener("click", () => {
+next.addEventListener("click", () => {
     modal.style.display = "none";
     page += 15;
     linkAPI = `https://pokeapi.co/api/v2/pokemon?limit=15&offset=${page}`;
     fetchPokemons();
     hideNextPrev();
 });
-// Función para ocultar/mostrar los botones de "previous" y "next"
 function hideNextPrev() {
     if (page == 0) {
-        previous === null || previous === void 0 ? void 0 : previous.classList.add("disabled");
+        previous.classList.add("disabled");
     }
     else {
-        previous === null || previous === void 0 ? void 0 : previous.classList.remove("disabled");
+        previous.classList.remove("disabled");
     }
 }
-// Función principal para obtener los Pokémon y sus detalles
 const fetchPokemons = async () => {
     try {
-        container.innerHTML = ""; // Limpiar el contenedor
-        const fragment = document.createDocumentFragment(); // Fragmento para optimizar la renderización
-        // Obtener los datos de la API
+        container.innerHTML = "";
+        // Elemento temporal de código
+        const fragment = document.createDocumentFragment();
         const response = await fetch(linkAPI);
         const data = await response.json();
-        // Recorrer los resultados y renderizar las tarjetas de Pokémon
         for (const pokemon of data.results) {
             const card = document.createElement("div");
-            card.classList.add("card");
-            card.textContent = pokemon.name; // Nombre básico
-            card.dataset.url = pokemon.url; // Guardar la URL para cargar detalles después
+            // Añado las clases propias de las Cards de Bootstrap
+            card.classList.add("card", "d-flex", "flex-colums", "justify-content-center", "align-items-center", "p-3");
+            card.textContent = pokemon.name;
+            card.dataset.url = pokemon.url;
             fragment.appendChild(card);
-            // Obtener los detalles de cada Pokémon (carga de detalles en segundo plano)
             try {
                 const detailsResponse = await fetch(pokemon.url);
                 const pokemonData = await detailsResponse.json();
                 const id = document.createElement("p");
-                id.classList.add("id");
+                id.classList.add("id", "bg-dark", "text-light", "px-2", "py-1", "rounded-pill");
                 id.textContent = "#" + pokemonData.id.toString().padStart(3, "0");
                 const image = document.createElement("img");
-                image.classList.add("image");
+                image.classList.add("image", "rounded-pill");
                 image.src = pokemonData.sprites.front_default;
                 const name = document.createElement("h3");
-                name.classList.add("name", "text-light");
+                name.classList.add("name", "text-light", "fs-6", "fw-bold");
                 name.textContent = pokemonData.name;
                 const contTypes = document.createElement("div");
-                contTypes.classList.add("cont-types");
+                contTypes.classList.add("cont-types", "d-flex", "gap-1");
                 const type1 = document.createElement("div");
                 const typeName1 = pokemonData.types[0].type.name;
                 type1.textContent = typeName1;
-                type1.classList.add(typeName1);
-                type1.classList.add("types");
-                type1.classList.add("py-0");
+                type1.classList.add(typeName1, "types", "py-0", "px-3", "rounded-pill", "text-light");
                 contTypes.appendChild(type1);
                 if (pokemonData.types.length > 1) {
                     const type2 = document.createElement("div");
                     const typeName2 = pokemonData.types[1].type.name;
                     type2.textContent = typeName2;
-                    type2.classList.add(typeName2);
-                    type2.classList.add("types");
-                    type2.classList.add("py-0");
+                    type2.classList.add(typeName2, "types", "py-0", "px-3", "rounded-pill", "text-light");
                     contTypes.appendChild(type2);
                 }
-                card.innerHTML = ""; // Limpiar la carta
+                card.innerHTML = "";
                 card.appendChild(id);
                 card.appendChild(image);
                 card.appendChild(name);
                 card.appendChild(contTypes);
             }
             catch (error) {
-                console.error("Error al cargar detalles del Pokémon:", error);
+                console.error("Error:", error);
             }
         }
-        container === null || container === void 0 ? void 0 : container.appendChild(fragment);
-        cardsModal(); // Llamar a la función para mostrar el modal
+        container.appendChild(fragment);
+        cardsModal();
     }
     catch (error) {
-        console.error("Error al obtener los datos:", error);
+        console.error("Error:", error);
     }
 };
-fetchPokemons(); // Cargar los Pokémon inicialmente
+fetchPokemons();
 // Crear Cartas para el Modal
 function cardsModal() {
     let cards = document.querySelectorAll(".card");
@@ -110,10 +101,8 @@ function cardsModal() {
             if (!pokemonUrl)
                 return;
             try {
-                // Realiza el fetch a la URL del Pokémon
                 const response = await fetch(pokemonUrl);
                 const pokemonData = await response.json();
-                // Extrae los datos necesarios
                 const id = pokemonData.id;
                 const name = pokemonData.name;
                 const image = pokemonData.sprites.other["official-artwork"].front_default;
@@ -124,10 +113,10 @@ function cardsModal() {
                 const abilities = pokemonData.abilities.map((ability) => ability.ability.name);
                 const stats = pokemonData.stats.map((stat) => `${stat.stat.name}: ${stat.base_stat} <div class="bar" style="width:${stat.base_stat * 1.2}px;"></div>`);
                 modal.innerHTML = `
-          <div class="personal-info">
+          <div class="personal-info d-flex flex-column text-light pb-2">
             <div class="close">x</div>
-            <div class="modal-id">#${id.toString().padStart(3, "0")}</div>
-            <div class="modal-name">${name}</div>
+            <div class="modal-id bg-dark text-light rounded-pill my-2">#${id.toString().padStart(3, "0")}</div>
+            <div class="modal-name text-light fs-4">${name}</div>
             <img class="modal-img" src="${image}" alt="${name}" />
             <p class="size">Weight: ${weight} kg / Height: ${height} m</p>
             <h2 class="modal-abilities">Abilities:</h2>
@@ -146,7 +135,7 @@ function cardsModal() {
                 modal.style.display = "flex";
                 // Cerrar el modal
                 let modalClose = document.querySelector(".close");
-                modalClose === null || modalClose === void 0 ? void 0 : modalClose.addEventListener("click", () => {
+                modalClose.addEventListener("click", () => {
                     modal.style.display = "none";
                 });
             }
--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,6 @@ let page: number = 0;
 
 let linkAPI: string = `https://pokeapi.co/api/v2/pokemon?limit=15&offset=${page}`;
 const container = document.getElementById("container-pkmn") as HTMLDivElement;
-
-// Variable global para pre-carga
-let nextPageData: Pokemon | null = null;
  
 let modal = document.createElement("div");
 modal.classList.add("modal");
@@ -59,6 +56,7 @@ const fetchPokemons = async () => {
   try {
     
     container.innerHTML = "";
+    // Elemento temporal de código
     const fragment = document.createDocumentFragment();
 
     const response = await fetch(linkAPI);
@@ -66,9 +64,13 @@ const fetchPokemons = async () => {
 
     for (const pokemon of data.results) {
       const card = document.createElement("div");
-      card.classList.add("card");
+
+      // Añado las clases propias de las Cards de Bootstrap
+      card.classList.add("card", "d-flex", "flex-colums", "justify-content-center", "align-items-center", "p-3");
       card.textContent = pokemon.name;
       card.dataset.url = pokemon.url;
+
+
       fragment.appendChild(card);
 
       try {
@@ -76,35 +78,31 @@ const fetchPokemons = async () => {
         const pokemonData = await detailsResponse.json();
 
         const id = document.createElement("p");
-        id.classList.add("id");
+        id.classList.add("id", "bg-dark", "text-light", "px-2", "py-1", "rounded-pill");
         id.textContent = "#" + pokemonData.id.toString().padStart(3, "0");
 
         const image = document.createElement("img");
-        image.classList.add("image");
+        image.classList.add("image", "rounded-pill");
         image.src = pokemonData.sprites.front_default;
 
         const name = document.createElement("h3");
-        name.classList.add("name", "text-light");
+        name.classList.add("name", "text-light", "fs-6", "fw-bold");
         name.textContent = pokemonData.name;
 
         const contTypes = document.createElement("div");
-        contTypes.classList.add("cont-types");
+        contTypes.classList.add("cont-types", "d-flex", "gap-1");
 
         const type1 = document.createElement("div");
         const typeName1 = pokemonData.types[0].type.name;
         type1.textContent = typeName1;
-        type1.classList.add(typeName1);
-        type1.classList.add("types");
-        type1.classList.add("py-0");
+        type1.classList.add(typeName1, "types", "py-0", "px-3", "rounded-pill", "text-light");
         contTypes.appendChild(type1);
 
         if (pokemonData.types.length > 1) {
           const type2 = document.createElement("div");
           const typeName2 = pokemonData.types[1].type.name;
           type2.textContent = typeName2;
-          type2.classList.add(typeName2);
-          type2.classList.add("types");
-          type2.classList.add("py-0");
+          type2.classList.add(typeName2, "types", "py-0", "px-3", "rounded-pill", "text-light");
           contTypes.appendChild(type2);
         }
 
@@ -114,7 +112,7 @@ const fetchPokemons = async () => {
         card.appendChild(name);
         card.appendChild(contTypes);
       } catch (error) {
-        console.error("Error al cargar detalles del Pokémon:", error);
+        console.error("Error:", error);
       }
     }
 
@@ -122,7 +120,7 @@ const fetchPokemons = async () => {
     cardsModal(); 
 
   } catch (error) {
-    console.error("Error al obtener los datos:", error);
+    console.error("Error:", error);
   }
 };
 
@@ -151,22 +149,17 @@ function cardsModal() {
         // Convertir a m
         const height = pokemonData.height / 10; 
 
-        // const abilities = pokemonData.abilities.map((ability: any) => ability.ability.name);
-
-        const abilities = pokemonData.abilities.forEach((element: any) => {
-          
-        });
-
+        const abilities = pokemonData.abilities.map((ability: any) => ability.ability.name);
 
         const stats = pokemonData.stats.map(
           (stat: any) => `${stat.stat.name}: ${stat.base_stat} <div class="bar" style="width:${stat.base_stat * 1.2}px;"></div>`
         );
 
         modal.innerHTML = `
-          <div class="personal-info">
+          <div class="personal-info d-flex flex-column text-light pb-2">
             <div class="close">x</div>
-            <div class="modal-id">#${id.toString().padStart(3, "0")}</div>
-            <div class="modal-name">${name}</div>
+            <div class="modal-id bg-dark text-light rounded-pill my-2">#${id.toString().padStart(3, "0")}</div>
+            <div class="modal-name text-light fs-4">${name}</div>
             <img class="modal-img" src="${image}" alt="${name}" />
             <p class="size">Weight: ${weight} kg / Height: ${height} m</p>
             <h2 class="modal-abilities">Abilities:</h2>
